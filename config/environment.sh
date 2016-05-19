b525dbc9cfab085c31b993f3c91622d06b7967a8
Heliozoa <dm89132@gmail.com>
2016-05-19 18:48:33 +0300

    Alustettu

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="kalleilv"
+USERNAME="madamada"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER="gudreads"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
