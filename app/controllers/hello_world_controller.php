ca512959bb5825a5e549ccd75f4e66fa18e94914
Heliozoa <dm89132@gmail.com>
2016-05-28 18:06:05 +0300

    Suunnitelmat

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -2,14 +2,50 @@
 
   class HelloWorldController extends BaseController{
 
-    public static function index(){
+    public static function sandbox(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  //View::make('home.html');
-	  echo '(Stereo Music Video)';
+      // Testaa koodiasi täällä
+			View::make('games.html');
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-	  View::make('helloworld.html');
+    public static function index(){
+   	  View::make('suunnitelmat/home.html');
     }
+		
+    public static function games(){
+			View::make('suunnitelmat/games.html');
+    }
+		
+		public static function tournaments(){
+			View::make('suunnitelmat/tournaments.html');
+		}
+		
+    public static function login(){
+			View::make('suunnitelmat/login.html');
+    }
+		
+    public static function register(){
+			View::make('suunnitelmat/register.html');
+    }
+		
+    public static function evo15(){
+			View::make('suunnitelmat/tournament_finished.html');
+    }
+		
+    public static function evo16(){
+			View::make('suunnitelmat/tournament_live.html');
+    }
+		
+    public static function game(){
+			View::make('suunnitelmat/game.html');
+    }
+		
+    public static function game_edit(){
+			View::make('suunnitelmat/game_edit.html');
+    }
+		
+    public static function tournament_edit(){
+			View::make('suunnitelmat/tournament_edit.html');
+    }
+		
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,39 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+  $routes->get('/games', function() {
+    HelloWorldController::games();
+  });
+  
+  $routes->get('/tournaments', function() {
+    HelloWorldController::tournaments();
+  });
+
+  $routes->get('/login', function() {
+    HelloWorldController::login();
+  });
+
+  $routes->get('/register', function() {
+    HelloWorldController::register();
+  });
+  
+  $routes->get('/evo16', function() {
+    HelloWorldController::evo16();
+  });
+  
+  $routes->get('/evo15', function() {
+    HelloWorldController::evo15();
+  });
+  
+  $routes->get('/tournament-edit', function() {
+    HelloWorldController::tournament_edit();
+  });
+  
+  $routes->get('/game', function() {
+    HelloWorldController::game();
+  });
+  
+  $routes->get('/game-edit', function() {
+    HelloWorldController::game_edit();
+  });
